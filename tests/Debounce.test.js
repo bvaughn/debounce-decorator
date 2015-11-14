548af7fb178bf0c4656e6309549c1767b9c6628e
Brian Vaughn <brian.david.vaughn@gmail.com>
2015-11-14 11:03:38 -0800

    Tidied up test

--- tests/Debounce.test.js
+++ tests/Debounce.test.js
@@ -15,70 +15,75 @@ describe('Debounce', () => {
     timers.useRealTimers()
   })
 
-  class Foo {
+  class MyClass {
     constructor () {
-      this._debouncedByDefaultCount = 0
-      this._debouncedBy100Count = 0
+      this._debouncedCount = 0
+      this._debouncedCustomCount = 0
     }
 
     @Debounce()
-    debouncedByDefault (value) {
-      this._debouncedByDefault = value
-      this._debouncedByDefaultCount++
+    debounced (value) {
+      this._debounced = value
+      this._debouncedCount++
     }
 
     @Debounce(100)
-    debouncedBy100 (value) {
-      this._debouncedBy100 = value
-      this._debouncedBy100Count++
+    debouncedCustom (value) {
+      this._debouncedCustom = value
+      this._debouncedCustomCount++
     }
   }
 
+  function assertDebounceState (instance, expectedValue, expectedCount = 0) {
+    expect(instance._debounced).to.equal(expectedValue)
+    expect(instance._debouncedCount).to.equal(expectedCount)
+  }
+
+  function assertDebounceCustomState (instance, expectedValue, expectedCount = 0) {
+    expect(instance._debouncedCustom).to.equal(expectedValue)
+    expect(instance._debouncedCustomCount).to.equal(expectedCount)
+  }
+
   it('should use a default debounce duration if no override is specified', () => {
-    const foo = new Foo()
-    foo.debouncedByDefault('foo')
-    expect(foo._debouncedByDefault).to.equal(undefined)
+    const instance = new MyClass()
+    instance.debounced('foo')
+    assertDebounceState(instance)
     timers.runTimersToTime(DEFAULT_DEBOUNCE_DURATION - 1)
-    expect(foo._debouncedByDefault).to.equal(undefined)
+    assertDebounceState(instance)
     timers.runTimersToTime(1)
-    expect(foo._debouncedByDefault).to.equal('foo')
-    expect(foo._debouncedByDefaultCount).to.equal(1)
+    assertDebounceState(instance, 'foo', 1)
   })
 
   it('should use a custom debounce duration if one is specified', () => {
-    const foo = new Foo()
-    foo.debouncedBy100('foo')
-    expect(foo._debouncedBy100).to.equal(undefined)
+    const instance = new MyClass()
+    instance.debouncedCustom('foo')
+    assertDebounceCustomState(instance)
     timers.runTimersToTime(99)
-    expect(foo._debouncedBy100).to.equal(undefined)
+    assertDebounceCustomState(instance)
     timers.runTimersToTime(1)
-    expect(foo._debouncedBy100).to.equal('foo')
-    expect(foo._debouncedBy100Count).to.equal(1)
+    assertDebounceCustomState(instance, 'foo', 1)
   })
 
   it('should queue up debounced events', () => {
-    const foo = new Foo()
-    foo.debouncedByDefault('foo')
-    foo.debouncedByDefault('bar')
-    expect(foo._debouncedByDefault).to.equal(undefined)
+    const instance = new MyClass()
+    instance.debounced('foo')
+    instance.debounced('bar')
+    assertDebounceState(instance)
     timers.runTimersToTime(DEFAULT_DEBOUNCE_DURATION)
-    expect(foo._debouncedByDefault).to.equal('bar')
-    expect(foo._debouncedByDefaultCount).to.equal(1)
-    foo.debouncedByDefault('foo')
-    foo.debouncedByDefault('baz')
+    assertDebounceState(instance, 'bar', 1)
+    instance.debounced('foo')
+    instance.debounced('baz')
     timers.runTimersToTime(DEFAULT_DEBOUNCE_DURATION)
-    expect(foo._debouncedByDefault).to.equal('baz')
-    expect(foo._debouncedByDefaultCount).to.equal(2)
+    assertDebounceState(instance, 'baz', 2)
   })
 
   it('should clear any pending debounced operations', () => {
-    const foo = new Foo()
-    foo.debouncedByDefault('foo')
-    expect(foo._debouncedByDefault).to.equal(undefined)
-    foo.debouncedByDefault.clear()
+    const instance = new MyClass()
+    instance.debounced('foo')
+    assertDebounceState(instance)
+    instance.debounced.clear()
     timers.runTimersToTime(DEFAULT_DEBOUNCE_DURATION)
-    expect(foo._debouncedByDefault).to.equal(undefined)
-    expect(foo._debouncedByDefaultCount).to.equal(0)
+    assertDebounceState(instance)
   })
 
   it('should decorate a plain function (not a class method)', () => {
@@ -105,25 +110,19 @@ describe('Debounce', () => {
   })
 
   it('should debounce class instances separately', () => {
-    const foo1 = new Foo()
-    const foo2 = new Foo()
-    foo1.debouncedByDefault('foo1-a')
-    foo2.debouncedByDefault('foo2-a')
-    expect(foo1._debouncedByDefault).to.equal(undefined)
-    expect(foo1._debouncedByDefaultCount).to.equal(0)
-    expect(foo2._debouncedByDefault).to.equal(undefined)
-    expect(foo2._debouncedByDefaultCount).to.equal(0)
-    timers.runTimersToTime(DEFAULT_DEBOUNCE_DURATION-1)
-    foo1.debouncedByDefault('foo1-b')
+    const instance1 = new MyClass()
+    const instance2 = new MyClass()
+    instance1.debounced('foo-1-a')
+    instance2.debounced('foo-2-a')
+    assertDebounceState(instance1)
+    assertDebounceState(instance2)
+    timers.runTimersToTime(DEFAULT_DEBOUNCE_DURATION - 1)
+    instance1.debounced('foo-1-b')
     timers.runTimersToTime(1)
-    expect(foo1._debouncedByDefault).to.equal(undefined)
-    expect(foo1._debouncedByDefaultCount).to.equal(0)
-    expect(foo2._debouncedByDefault).to.equal('foo2-a')
-    expect(foo2._debouncedByDefaultCount).to.equal(1)
-    timers.runTimersToTime(DEFAULT_DEBOUNCE_DURATION-1)
-    expect(foo1._debouncedByDefault).to.equal('foo1-b')
-    expect(foo1._debouncedByDefaultCount).to.equal(1)
-    expect(foo2._debouncedByDefault).to.equal('foo2-a')
-    expect(foo2._debouncedByDefaultCount).to.equal(1)
+    assertDebounceState(instance1)
+    assertDebounceState(instance2, 'foo-2-a', 1)
+    timers.runTimersToTime(DEFAULT_DEBOUNCE_DURATION - 1)
+    assertDebounceState(instance1, 'foo-1-b', 1)
+    assertDebounceState(instance2, 'foo-2-a', 1)
   })
 })
